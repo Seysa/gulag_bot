b5e852602235de374b060ac89bf575e59372deda
garion.goubard <garion.goubard@etu.u-bordeaux.fr>
2021-02-17 12:43:03 +0100

    reworked log to work on files for more than 2000 characters (discord message limit)

--- commands/log.js
+++ commands/log.js
@@ -2,6 +2,24 @@ const permissions = require(`../utils/permissions`);
 const fs = require(`fs`);
 const { safeDelete } = require(`../utils/message_utils`);
 
+const MAX_LENGTH = 2000 - 6; // backquotes
+
+function split_at_index(value, index){
+	return [value.substring(0, index), value.substring(index)];
+}
+
+function getIndexOfLastLine(string){
+	let newLineLastPos = 0;
+	for(let i = 0; i < MAX_LENGTH; i++){
+		if(string[i] === '\n'){
+			newLineLastPos = i;
+		}
+	}
+	return newLineLastPos;
+	
+	
+}
+
 module.exports = {
 	name: `log`,
 	description: `Shows all server logs`,
@@ -10,12 +28,22 @@ module.exports = {
 	async execute(client, message, _args) {
 		safeDelete(message);
 		try {
-			const data = fs.readFileSync(`output.log`, `utf8`);
-			if(!data) {
-				await message.channel.send(`log file is empty`);
-			}
-			else {
-				await message.channel.send(data);
+			const data = fs.readFileSync('/home/pi/gulag_bot/output.log', 'utf8');
+			if(!data){
+				await message.channel.send("Output file is empty");
+			} else {
+				if(data.length > MAX_LENGTH){
+					let toAnalyze = data;
+					while(toAnalyze.length > MAX_LENGTH){
+						let splitted = split_at_index(toAnalyze, getIndexOfLastLine(toAnalyze));
+						await message.channel.send("```" + splitted[0] + "```");
+						console.log(splitted[0]);
+						toAnalyze = splitted[1];
+					}
+					await message.channel.send("```" + toAnalyze + "```");
+				} else {
+					await message.channel.send("```" + data + "```");
+				}
 			}
 		}
 		catch(e) {
